687fab0f46522013aca2c71e71b163996f543956
Tayron Miranda <contato@tayron.com.br>
2020-05-12 08:43:33 -0300

    Atualizando scripts

---
+++ Servidor/DDOS.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {1..5000}
+do
+   wget -O/dev/null http://constance.local
+done
---
+++ Utilidade Linux/limparCacheRede.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo service network-manager restart
+
+echo
+read -s -n 1 -p "Pressione qualquer tecla para continuar . . ."
+echo
